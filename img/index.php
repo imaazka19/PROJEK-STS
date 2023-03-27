5024bd507efdcdcd2b8f45ea46d9e159d5ef66d9
fifitt <tiarabintang@gmail.com>
2023-03-27 08:23:01 +0700

    first commit

---
+++ img/index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charshet="UTF-8">
+        <meta http-equiv="X-UA-compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+        <link rel="stylesheet" href="style.css">
+        <title>portofolio</title>
+    </head>
+    <body>
+        <div class="hero">
+            <nav>
+                <h2 class="logo">porto<span>folio</span></h2>
+                <ul>
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">tentang kami</a></li>
+                    <li><a href="#">servis</a></li>
+                    <li><a href="#">kontak</a></li>
+                </ul>
+                <div class="btn">subscribe</div>
+            </nav>
+            <div class="content">
+                <h4>Hello! Nama saya</h4>
+                <h1>Ima Azka Rosadah <span>Ima</span></h1>
+                <h3>Saya seorang pelajar SMKN 1 Sayung</h1>
+                    <div class="news">
+                        <form action="">
+                            <input type="email" name="email" placeholder="input your email" id="mail">
+                            <input type="submit" value="Lets To Start" name="submit">
+                        </form>
+                    </div>
+            </div>
+    </div>
+    <div class="about">
+        <div class="main">
+            <img src="img/pro.jpg" alt="">
+            <div class="about-text">
+                <h2>Tentang Kami</h2>
+                <h5>Developer<span>& Designer</h5>
+                    <p> saya adalah seorang progammer dan desainer di perusahaan ternama di indonesia</p>
+                    <button type="button">Let's Talk</button>
+        </div>
+    </div>
+            </div>
+        <div class="servise">
+            <div class="title">
+                <h2>servis</h2>
+            </div>
+            <div class="box">
+                <div class="card">
+                <i class="fa-solid fa-bars"></i>
+                    <h5>Web Development</h5>
+                    <div class="pra">
+                        <p>mengerjakan jasa web termasuk dari keahlian kami</p>
+                        <p style="text-align: center;"></p>
+                        <a href="#" class="button">Read More</a>
+                    </div>
+                </div>
+                    <div class="card">
+                        <i class="fa-regular fa-user"></i>
+                        <h5>Desain Grafis</h5>
+                        <div class="pra">
+                            <p>Selainsebagai Web Development saya juga mahir dalan desain grafis</p>
+                            <p style="text-align: center;"></p>
+                            <a href="#" class="button">Read More</a>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <i class="fa-regular fa-desktop"></i>
+                        <h5>Android Development</h5>
+                        <div class="pra">
+                            <p>saya juga ahli dalam bidang android Development</p>
+                            <p style="text-align: center;"></p>
+                            <a href="#" class="button">Read More</a>
+                        </div>
+                    </div>
+            </div>
+        </div>
+        <div class="kontak-saya">
+            <p>lihat lebih jauh tentang saya</p>
+            <a href="#" class="button-dua">Hubungi Kami</a>
+        </div>
+        <footer>
+            <p>Ima Azka Rosadah</p>
+            <p>Untuk lebih mengenaltentang saya hubungi</p>
+            <div class="sosial">
+                <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+            </div>
+            <p class="end">CopyRight@ imaazkarosaazka.com</p>
+        </footer>
+
+    </body>
+</html>
